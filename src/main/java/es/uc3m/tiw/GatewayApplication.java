68a3b6225b9e51cdc468c567e50edb9bd24d1961
Beaacosta <100304636@alumnos.uc3m.es>
2016-12-13 10:53:55 +0000

    arreglado error restTemplate

--- src/main/java/es/uc3m/tiw/GatewayApplication.java
+++ src/main/java/es/uc3m/tiw/GatewayApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class GatewayApplication extends SpringBootServletInitializer{
@@ -12,4 +15,15 @@ public class GatewayApplication extends SpringBootServletInitializer{
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GatewayApplication.class);
+	}
+
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 }
+
